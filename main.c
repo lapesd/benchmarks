c2e5477664f9b183931ff9974ec32416b14b412c
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-09-09 18:15:19 -0300

    fixing build errors

--- main.c
+++ main.c
@@ -20,6 +20,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <omp.h>
 
 #define NITERATIONS 5
@@ -31,16 +32,7 @@
 
 #define MATRIX(m, i, j) m[(i)*n + (j)]
 
-/*
- * @brief Matrix size.
- */
-static int n = 0;
-
-static double *a;
-static double *b;
-static double *c;
-
-void mult1(void)
+void matrix_mult1(double *restrict c, const double *restrict a, const double *restrict b, int n)
 {
 	#pragma omp parallel for
 	for (int i = 0; i < n; i++)
@@ -53,7 +45,7 @@ void mult1(void)
 	}
 }
 
-void mult2(void)
+void matrix_mult2(double *restrict c, const double *restrict a, const double *restrict b, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -73,7 +65,7 @@ void mult2(void)
  * @param a Sparse matrix.
  * @param b Dense matrix.
  */
-static void sparsematrix_mult(restrict double *c, restrict const double *a, restrict const double *b)
+static void sparsematrix_mult(double *restrict c, const double *restrict a, const double *restrict b, int n)
 {
 	#pragma omp parallel for schedule(dynamic, 1)
 	for (int i = 0; i < n; i++)
@@ -100,11 +92,23 @@ static void usage(void)
 }
 
 /**
- * @brief Initializes a matrix.
+ * @brief Creates a matrix.
  *
  * @param m Target matrix.
  */
-static void matrix_init(double *m)
+static double *matrix_create(int n)
+{
+	double *m;
+
+	m = malloc(n*n*sizeof(double));
+	assert(m != NULL); 
+
+	memset(m, 0, n*n*sizeof(double));
+
+	return (m);
+}
+
+static void matrix_init(double *m, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -118,8 +122,20 @@ static void matrix_init(double *m)
  *
  * @param m Target matrix.
  */
-static void sparsematrix(double *m)
+static double *sparsematrix_create(int n)
 {	
+	double *m;
+
+	m = malloc(n*n*sizeof(double));
+	assert(m != NULL); 
+
+	memset(m, 0, n*n*sizeof(double));
+
+	return (m);	
+}
+
+static void sparsematrix_init(double *m, int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
@@ -136,6 +152,14 @@ static void sparsematrix(double *m)
 int main(int argc, char **argv)
 {
 	double start, end;
+	int n = 0;
+
+	double *a1;
+	double *a2;
+	double *b;
+	double *c1;
+	double *c2;
+	double *c3;
 
 	if (argc < 2)
 		usage();
@@ -146,20 +170,18 @@ int main(int argc, char **argv)
 	/* Sanity check. */
 	assert(n > 0);
 
-	/* Create matrices. */
-	assert((a = malloc(n*n*sizeof(double))) != NULL);
-	assert((b = malloc(n*n*sizeof(double))) != NULL);
-	assert((c = malloc(n*n*sizeof(double))) != NULL);
-
-	matrix_init(a);
-	matrix_init(b);
-	matrix_init(c);
+	a1 = matrix_create(n); matrix_init(a1, n);
+	b  = matrix_create(n); matrix_init(b,  n);
+	c1 = matrix_create(n); matrix_init(c1, n);
+	c2 = matrix_create(n); matrix_init(c2, n);
+	c3 = matrix_create(n); matrix_init(c3, n);
+	a2 = sparsematrix_create(n); sparsematrix_init(a2, n);
 	
 	/* Benchmark 1. */
 	for (int it = 0; it <= NITERATIONS; it++)
 	{
 		start = omp_get_wtime();
-		mult1();
+		matrix_mult1(c1, a1, b, n);
 		end = omp_get_wtime();
 		if (it)
 			printf("mult1: %lf\n", end - start);
@@ -169,11 +191,28 @@ int main(int argc, char **argv)
 	for (int it = 0; it <= NITERATIONS; it++)
 	{
 		start = omp_get_wtime();
-		mult2();
+		matrix_mult2(c2, a1, b, n);
 		end = omp_get_wtime();
 		if (it)
 			printf("mult2: %lf\n", end - start);
 	}
 	
+	/* Benchmark 3. */
+	for (int it = 0; it <= NITERATIONS; it++)
+	{
+		start = omp_get_wtime();
+		sparsematrix_mult(c3, a2, b, n);
+		end = omp_get_wtime();
+		if (it)
+			printf("sparsemult: %lf\n", end - start);
+	}
+	
+	/* House keeping. */
+	free(a1);
+	free(b);
+	free(c1);
+	free(c2);
+	free(c3);
+
 	return (EXIT_FAILURE);
 }
